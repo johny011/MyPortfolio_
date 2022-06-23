c07ed4d1de5fdc0728c08d1036303300aff8d3e3
johny eshak <johny.eshak@hotmail.com>
2022-06-23 23:23:03 +0200

    Add project files.

---
+++ Core/Entites/EntityBase.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Core.Entites
+{
+    public class EntityBase
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ Core/Entites/PortfolioItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entites
+{
+    public class PortfolioItem :EntityBase
+    {
+        public string ProjectName { get; set; }
+        public string Descrition { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
